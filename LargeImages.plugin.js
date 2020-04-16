bef4b539389e9df413cd9eda129c255fb5615c68
seymar <seymar@users.noreply.github.com>
2020-04-17 00:20:53 +0200

    Use proxied image instead of original

--- LargeImages.plugin.js
+++ LargeImages.plugin.js
@@ -15,7 +15,7 @@ var LargeImages = (() => {
             authors:[
                 { name: 'seymar' }
             ],
-            version: '0.0.3',
+            version: '0.0.4',
             description: 'Show large full resolution images in chats',
             github: 'https://github.com/seymar/LargeImages',
             github_raw: 'https://raw.githubusercontent.com/seymar/LargeImages/master/LargeImages.plugin.js'
@@ -70,8 +70,7 @@ var LargeImages = (() => {
             img.style.height = ''
             
             // Increase resolution by loading original image
-            if (!img.parentNode) return false
-            const newUrl = img.parentNode.getAttribute('href')
+            const newUrl = img.src.replace(/\?.*/, '') // Remove height/width parameters
 
             if (img.src == newUrl) return false
             

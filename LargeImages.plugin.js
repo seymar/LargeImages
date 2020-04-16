62bebf956ad3d1160d50422e813b484a894c0ecb
seymar <seymar@users.noreply.github.com>
2020-04-16 23:42:19 +0200

    Oops fix

--- LargeImages.plugin.js
+++ LargeImages.plugin.js
@@ -15,7 +15,7 @@ var LargeImages = (() => {
             authors:[
                 { name: 'seymar' }
             ],
-            version: '0.0.1',
+            version: '0.0.3',
             description: 'Show large full resolution images in chats',
             github: 'https://github.com/seymar/LargeImages',
             github_raw: 'https://raw.githubusercontent.com/seymar/LargeImages/master/LargeImages.plugin.js'
@@ -65,8 +65,8 @@ var LargeImages = (() => {
         
             // Stretch image itself
             img.style.position = 'relative'
-            img.style.width = ''
             img.style.maxWidth = '100%'
+            img.style.width = ''
             img.style.height = ''
             
             // Increase resolution by loading original image
@@ -75,7 +75,7 @@ var LargeImages = (() => {
 
             if (img.src == newUrl) return false
             
-            LOG('Image enlarged from/to: ' + (img.src.length - newUrl.length), img.src, newUrl)
+            LOG('Enlarging image', img.src, newUrl)
             img.src = newUrl
         }
     }
